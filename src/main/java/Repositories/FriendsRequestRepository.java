d132a1a399758ba057990f6c365e4070d3673bee
SteinmannTh <steinmann.thms@gmail.com>
2018-11-10 13:04:15 +0000

    changed Repository to CrudRepository in FriendRepository

--- src/main/java/Repositories/FriendsRequestRepository.java
+++ src/main/java/Repositories/FriendsRequestRepository.java
@@ -5,11 +5,12 @@ import EntityClasses.FriendRequestCompositeKey;
 import EntityClasses.FriendsRequest;
 import java.util.List;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.Repository;
 import org.springframework.data.repository.query.Param;
 
 @org.springframework.stereotype.Repository
-public interface FriendsRequestRepository extends Repository<FriendsRequest, FriendRequestCompositeKey> {
+public interface FriendsRequestRepository extends CrudRepository<FriendsRequest, FriendRequestCompositeKey> {
     
     @Query("select r from FriendsRequest r where r.request_id.friendUser = :user")
     List<FriendsRequest>getIncomingRequests(@Param("user") String userName);
@@ -17,7 +18,7 @@ public interface FriendsRequestRepository extends Repository<FriendsRequest, Fri
     @Query("select r from FriendsRequest r where r.request_id.requestingUser = :user")
     List<FriendsRequest>getOutgoingRequests(@Param("user") String userName);
 
-    FriendsRequest save(FriendsRequest request);
+    //FriendsRequest save(FriendsRequest request);
 
-    void deleteById(FriendRequestCompositeKey id);
+    //void deleteById(FriendRequestCompositeKey id);
 }
