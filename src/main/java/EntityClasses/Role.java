bcac6598a67ffd1b4bba8d0d6554c7ea96c62455
steinmann-th <steinmann.thms@gmail.com>
2018-11-12 22:52:00 +0400

    time to implement registration and logging in

---
+++ src/main/java/EntityClasses/Role.java
@@ -0,0 +1,21 @@
+package EntityClasses;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Role {
+    
+    @Id
+    private String role;
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+    
+    
+}
---
+++ src/main/java/Repositories/RoleRepository.java
@@ -0,0 +1,10 @@
+package Repositories;
+
+import EntityClasses.Role;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends CrudRepository<Role, String> {
+    
+}
