d1baff00533ec37034712aced8cc19ae6e2ff989
pr0p1k <propiktunes@gmail.com>
2018-11-25 14:31:44 +0300

    Added /me for user cabinet and fixed messaging

--- src/main/java/com/p3212/configuration/CommunicationController.java
+++ src/main/java/com/p3212/configuration/CommunicationController.java
@@ -10,6 +10,7 @@ import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -45,8 +46,12 @@ public class CommunicationController {
 
     @GetMapping("/messages/unread")
     public @ResponseBody
-    List<PrivateMessage> getUnreadMessages(@RequestBody User user) {
-        return messageServ.getUnreadMessages(user.getLogin());
+    List<PrivateMessage> getUnreadMessages() {
+        return messageServ
+                .getUnreadMessages(
+                        userServ.getUser(
+                                SecurityContextHolder.getContext()
+                                        .getAuthentication().getName()));
     }
 
     @GetMapping("/messages/dialog")
